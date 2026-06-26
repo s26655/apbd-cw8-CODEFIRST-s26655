16960956428cefb61970b6095ba5f39abbbb185d
s26655 <235947034+s26655@users.noreply.github.com>
2026-06-26 23:42:55 +0200

    feat: update library navigation

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
         name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
+        pattern: "{controller=Books}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.Run();
